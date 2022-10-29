084f9929711915d1697ae2b71cb7238140ae9686
amanjit-gill-data <amanjitgilldata@gmail.com>
2022-10-30 05:11:43 +1100

    add q3c calcs

--- quiz1/quiz1.R
+++ quiz1/quiz1.R
@@ -68,7 +68,28 @@ minor_xvals <- c(minor_pt1[1], minor_pt2[1])
 minor_yvals <- c(minor_pt1[2], minor_pt2[2])
 
 # plot major and minor axes
-plot(c(major_xvals,minor_xvals), c(major_yvals,minor_yvals), pch=4, col="blue", lwd=2)
+plot(c(major_xvals,minor_xvals), c(major_yvals,minor_yvals), pch=4, col="blue", lwd=2, xlim=c(-5,2), ylim=c(-7,-1))
 lines(major_xvals, major_yvals)
 lines(minor_xvals, minor_yvals)
 points(xbar[1], xbar[2], pch=16, col="blue")
+
+# PART C - projection to get confidence interval
+
+scale <- p*(n-1)*qf(1-alpha, p, n-p)/(n*(n-p))
+
+# for variable x1
+l_x1 <- c(1,0)
+x1_ci_low <- c(crossprod(l_x1, xbar)) - sqrt(scale * t(l_x1) %*% S %*% l_x1)
+x1_ci_high <- c(crossprod(l_x1, xbar)) + sqrt(scale * t(l_x1) %*% S %*% l_x1)
+
+# for variable x2
+l_x2 <- c(0,1)
+x2_ci_low <- c(crossprod(l_x2, xbar)) - sqrt(scale * t(l_x2) %*% S %*% l_x2)
+x2_ci_high <- c(crossprod(l_x2, xbar)) + sqrt(scale * t(l_x2) %*% S %*% l_x2)
+
+# plot x1 CI as vertical lines
+# plot x2 CI as horizontal lines
+abline(v=c(x1_ci_low, x1_ci_high))
+abline(h=c(x2_ci_low, x2_ci_high))
+
+
